5a4eb9b205b4646db366adf621b7085eb1cc5fa1
Mathias Ferniss <5211966@RTS-365.dk>
2020-08-28 12:09:38 +0200

    offline virker

--- dist/sw.js
+++ dist/sw.js
@@ -1,72 +1,39 @@
-let cacheName = 'static-cache-v1';
-let filesToCache = [
-    '/',
-    '/fallback.html',
-    '/index.html',
-    '/assets/images/facebook.jpg',
-    '/assets/images/hifi.JPG',
-    '/assets/images/mario.png',
-    '/assets/images/marioios.png',
-    '/assets/images/music.JPG',
-    '/assets/images/newsbox.JPG',
-    '/assets/images/programming.JPG',
-    '/assets/images/spil.JPG',
-    '/assets/css/website-container.css',
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-//   '/css/styles.css',
-//   '/js/scripts.js',
-//   '/images/logo.svg',
+//registerRoute(({ request })=> request.destination !== 'script', new NetworkFirst());
+const { registerRoute } = workbox.routing;
+const { precacheAndRoute } = workbox.precaching;
+const { StaleWhileRevalidate } = workbox.strategies;
+const { setCatchHandler } = workbox.routing;
 
-  
+precacheAndRoute([
+    { url: '/', revision: null },
+    { url: '/fallback.html', revision: null },
+    { url: '/index.html', revision: null },
+    { url: '/assets/images/facebook.jpg', revision: null },
+    { url: '/assets/images/hifi.JPG', revision: null },
+    { url: '/assets/images/mario.png', revision: null },
+    { url: '/assets/images/marioios.png', revision: null },
+    { url: '/assets/images/music.JPG', revision: null },
+    { url: '/assets/images/newsbox.JPG', revision: null },
+    { url: '/assets/images/programming.JPG', revision: null },
+    { url: '/assets/images/spil.JPG', revision: null },
+    { url: '/assets/css/website-container.css', revision: null },
+])
 
-//   '/',
-];
+registerRoute(({ url }) => url.pathname.startsWith('/'), new StaleWhileRevalidate());
 
-self.addEventListener('install', function(event) {
-  console.log('service worker installed');
-  event.waitUntil(caches.open(cacheName)
-        .then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-    cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('install', function(event) {
-    console.log('service worker installed')
-  });
-
-
-
-
-self.addEventListener('activate', function(event) {
-    console.log('activate', event)
-    event.waitUntil(
-        caches.keys().then(function(keys){
-            console.log(keys);
-            if(event === cacheName) caches.delete(event)
-            // return Promise.all(keys.filter((key) => key === cacheName).map(key => caches.delete(key)))
-        })
-    )
+setCatchHandler(({url, event, params}) => {
+    if (event.request.destination === 'document') {
+        return caches.match('/fallback.html');
+    }else{
+        return Response.error();
+    }
 })
 
-self.addEventListener('fetch', function(event) {
-    console.log('fetch', event)
-    event.respondWith(
-        caches.open(cacheName).then(function(cache){
-            return cache.match(event.request).then(function(response){
-                return (
-                    response || fetch(event.request)
-                    .then(function(response) {
-                        cache.put(event.request, response.clone())
-                        return response;
-                    })
-                );
-            })
-            .catch(function(){
-                return caches.match('/fallback.html')
-            })
-         
-        })
-    )
-})
+
+// if (workbox) {
+//   console.log(`Yay! Workbox is loaded 🎉`);
+// } else {
+//   console.log(`Boo! Workbox didn't load 😬`);
+// }
