1f8214fc555a5e2a27200eaae24ec7cf8bfd189c
Mathias Ferniss <5211966@RTS-365.dk>
2020-09-01 13:27:07 +0200

    prøver gør så man kun kan trykke en gang

--- src/javascript/quiz.js
+++ src/javascript/quiz.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Det her er her hvor jeg laver start knappen til at virke
     let quizArticle = document.querySelector('.quiz-article');
     let quizstart = document.querySelector('.quizStart');
     quizstart.addEventListener('click', ()=>{
+    //her får det første spørgsmål frem når man trykker på start på knappen
         quizArticle.innerHTML = `
         <section>
         <h2>Hvornår startede 2 verdenskrig</h2>
@@ -11,14 +13,27 @@ document.addEventListener('DOMContentLoaded', () => {
         <button class="false">1933</button>
         <button class="true">1939</button>
         <button class="false">1940</button>
-        <button class="next">Næste Spørgsmål</button>
+        <button class="next1">Næste Spørgsmål</button>
         </section>
         `
+        //Her får jeg det rigtige svar til at blive blot
+        
         let sandt = document.querySelector('.true')
+        let falsk = document.querySelectorAll('.false')
         sandt.addEventListener('click', ()=>{
             sandt.style.backgroundColor = "lightgreen";
+            if (sandt.style.backgroundColor = "lightgreen") {
+                falsk.style.visibilty = "hidden";
+            }
         })
-        document.querySelector('.next').addEventListener('click', ()=>{
+        falsk.forEach(element => {
+            element.addEventListener('click', () =>{
+                element.style.backgroundColor = "red";
+            })
+        });
+
+        //Her starter det andet spørgsmål
+        document.querySelector('.next1').addEventListener('click', ()=>{
 
             if (sandt.style.backgroundColor = "lightgreen") {
                 quizArticle.innerHTML = `
@@ -28,12 +43,41 @@ document.addEventListener('DOMContentLoaded', () => {
             <section>
                 <button class="false">25</button>
                 <button class="false">30</button>
-                <button class="true">50</button>
                 <button class="false">55</button>
-                <button>Næste Spørgsmål</button>
+                <button class="true">50</button>
+                <button class="next2">Næste Spørgsmål</button>
                 </section>
                 `
+                // her bliver det rigtige svar blåt
+                let sandt = document.querySelector('.true')
+                sandt.addEventListener('click', ()=>{
+                    sandt.style.backgroundColor = "lightgreen";
+                })
             }
+
+
+            // Her starter spørgsemål nr 3
+            document.querySelector('.next2').addEventListener('click', ()=>{
+                if (sandt.style.backgroundColor = "lightgreen") {
+                    quizArticle.innerHTML = `
+                    <section>
+                    <h2>Hvad er h2o</h2>
+                </section>
+                <section>
+                    <button class="true">vand</button>
+                    <button class="false">ild</button>
+                    <button class="false">jord</button>
+                    <button class="false">luft</button>
+                    <button class="next2">Næste Spørgsmål</button>
+                    </section>
+                    `
+                    // her bliver det rigtige svar blåt
+                    let sandt = document.querySelector('.true')
+                    sandt.addEventListener('click', ()=>{
+                        sandt.style.backgroundColor = "lightgreen";
+                    })
+                }
+            })
         })
     })
 })
