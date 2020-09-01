3b04835c1fc03e6f1129f9118c51e9fe510af1c7
Mathias Ferniss <5211966@RTS-365.dk>
2020-09-01 11:10:58 +0200

    Den går videre til det næste spørgsemål

--- dist/assets/javascript/quiz.js
+++ dist/assets/javascript/quiz.js
@@ -4,6 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
   var quizArticle = document.querySelector('.quiz-article');
   var quizstart = document.querySelector('.quizStart');
   quizstart.addEventListener('click', function () {
-    quizArticle.innerHTML = "\n        <section>\n        <h2>Hvorn\xE5r startede 2 verdenskrig</h2>\n    </section>\n    <section>\n        <p>1930</p>\n        <p>1933</p>\n        <p>1939</p>\n        <p>1940</p>\n        </section>\n        <button>N\xE6ste sp\xF8rgsm\xE5l</button>\n        ";
+    quizArticle.innerHTML = "\n        <section>\n        <h2>Hvorn\xE5r startede 2 verdenskrig</h2>\n    </section>\n    <section>\n        <button class=\"false\">1930</button>\n        <button class=\"false\">1933</button>\n        <button class=\"true\">1939</button>\n        <button class=\"false\">1940</button>\n        <button class=\"next\">N\xE6ste Sp\xF8rgsm\xE5l</button>\n        </section>\n        ";
+    var sandt = document.querySelector('.true');
+    sandt.addEventListener('click', function () {
+      sandt.style.backgroundColor = "lightgreen";
+    });
+    document.querySelector('.next').addEventListener('click', function () {
+      if (sandt.style.backgroundColor = "lightgreen") {
+        quizArticle.innerHTML = "\n                <section>\n                <h2>Hvad er 5*5+5</h2>\n            </section>\n            <section>\n                <button class=\"false\">25</button>\n                <button class=\"false\">30</button>\n                <button class=\"true\">50</button>\n                <button class=\"false\">55</button>\n                <button>N\xE6ste Sp\xF8rgsm\xE5l</button>\n                </section>\n                ";
+      }
+    });
   });
 });
--- src/javascript/quiz.js
+++ src/javascript/quiz.js
@@ -7,12 +7,33 @@ document.addEventListener('DOMContentLoaded', () => {
         <h2>Hvornår startede 2 verdenskrig</h2>
     </section>
     <section>
-        <p>1930</p>
-        <p>1933</p>
-        <p>1939</p>
-        <p>1940</p>
+        <button class="false">1930</button>
+        <button class="false">1933</button>
+        <button class="true">1939</button>
+        <button class="false">1940</button>
+        <button class="next">Næste Spørgsmål</button>
         </section>
-        <button>Næste spørgsmål</button>
         `
+        let sandt = document.querySelector('.true')
+        sandt.addEventListener('click', ()=>{
+            sandt.style.backgroundColor = "lightgreen";
+        })
+        document.querySelector('.next').addEventListener('click', ()=>{
+
+            if (sandt.style.backgroundColor = "lightgreen") {
+                quizArticle.innerHTML = `
+                <section>
+                <h2>Hvad er 5*5+5</h2>
+            </section>
+            <section>
+                <button class="false">25</button>
+                <button class="false">30</button>
+                <button class="true">50</button>
+                <button class="false">55</button>
+                <button>Næste Spørgsmål</button>
+                </section>
+                `
+            }
+        })
     })
 })
